8e37510c02c1c70e76d960c0a7f1763bb0db2221
Tree Star Systems <info@treestarsystems.com>
2025-04-19 14:05:01 -0400

    establish email package

---
+++ email/of365.go
@@ -0,0 +1,16 @@
+package email
+
+/*
+ Send/Receive emails via GraphQL: https://learn.microsoft.com/en-us/graph/outlook-create-send-messages
+ Endpoint
+  - Message: https://learn.microsoft.com/en-us/graph/api/resources/message?view=graph-rest-1.0
+  - AA:
+    - https://learn.microsoft.com/en-us/graph/api/resources/message?view=graph-rest-1.0
+	- https://learn.microsoft.com/en-us/exchange/client-developer/legacy-protocols/how-to-authenticate-an-imap-pop-smtp-application-by-using-oauth
+ Example
+  - Create a MS Graph client: https://learn.microsoft.com/en-us/graph/sdks/create-client?tabs=go
+  - https://learn.microsoft.com/en-us/answers/questions/936444/how-to-send-an-email-with-multiple-contents-using
+  - https://stackoverflow.com/questions/64481592/sending-office-365-email
+ Packages
+  - https://pkg.go.dev/github.com/recolabs/go-office365
+*/
---
+++ email/smtp.go
@@ -0,0 +1,9 @@
+package email
+
+/*
+ Research
+  - https://zetcode.com/golang/email-smtp/
+  - https://mailtrap.io/blog/golang-send-email/
+  - https://www.geeksforgeeks.org/sending-email-using-smtp-in-golang/
+  - https://www.loginradius.com/blog/engineering/sending-emails-with-golang/
+*/
