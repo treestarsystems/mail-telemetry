9da8443df5cf771c88ede68cae2348d7d813883d
Tree Star Systems <info@treestarsystems.com>
2025-05-08 01:52:13 -0400

    added potential MD5 code, need to review/modify for use

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -23,4 +23,18 @@ func InitTasks() {
 		}
 	}
 	fmt.Println(scenarioCount)
+
+	// TODO: write all scenario instances to it's own table with a MD5 hash of the struct.
+	// GenerateMD5HashOfStruct generates an MD5 hash of a given struct.
+	// func GenerateMD5HashOfStruct(data interface{}) (string, error) {
+	// 	// Convert the struct to JSON
+	// 	jsonData, err := json.Marshal(data)
+	// 	if err != nil {
+	// 		return "", fmt.Errorf("error - Failed to marshal struct to JSON: %v", err)
+	// 	}
+
+	//		// Generate MD5 hash
+	//		hash := md5.Sum(jsonData)
+	//		return hex.EncodeToString(hash[:]), nil
+	//	}
 }
